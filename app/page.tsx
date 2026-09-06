81de03ebd84a76fc7dc42f9431d09a09e2ce8b2a
Taha Can Tilkioglu <101402331+tilkitaha@users.noreply.github.com>
2026-09-06 14:59:23 +0200

    App Store: surface privacy and support from game

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import V3WorldApp from "@/components/world/v3-world-app";
+import { AppStoreLinks } from "@/components/world/app-store-links";
 import "./v3.css";
 
-export default function Home() { return <V3WorldApp />; }
+export default function Home() {
+  return <>
+    <V3WorldApp />
+    <AppStoreLinks />
+  </>;
+}
